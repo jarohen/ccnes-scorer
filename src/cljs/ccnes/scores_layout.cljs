4be5583294e3125a83b71916d7abcf0ec434b35c
James Henderson <james@pcr.me.uk>
2013-07-21 17:08:24 +0100

    Displaying missing answers

--- src/cljs/ccnes/scores_layout.cljs
+++ src/cljs/ccnes/scores_layout.cljs
@@ -1,6 +1,7 @@
 (ns ccnes.scores-layout
   (:require [dommy.core :as d]
-            [ccnes.questions :refer [sub-scale-name sub-scale-order]])
+            [ccnes.questions :refer [sub-scale-name sub-scale-order]]
+            [clojure.string :as s])
   (:require-macros [dommy.macros :refer [node]]))
 
 (defn order-scores [scores]
@@ -9,18 +10,28 @@
 
 (defn scores-node [scores]
   (let [{:keys [scores missing]} scores]
-    (when scores
-      (node
-       (list
-        [:h4 "Scores:"]
-        [:table {:style {:margin-bottom :2em}}
-         (for [{:keys [sub-scale score]} (order-scores scores)]
-           [:tr
-            [:td
-             [:b (sub-scale-name sub-scale)]
-             ": "]
-            [:td {:style {:padding-left :1em}}
-             (format "%.2f" score)]])])))))
+    (cond
+
+     scores
+     (node
+      (list
+       [:h4 "Scores:"]
+       [:table {:style {:margin-bottom :2em}}
+        (for [{:keys [sub-scale score]} (order-scores scores)]
+          [:tr
+           [:td
+            [:b (sub-scale-name sub-scale)]
+            ": "]
+           [:td {:style {:padding-left :1em}}
+            (format "%.2f" score)]])]))
+
+     missing
+     (node
+      [:div {:style {:color :#b00}}
+       [:h4 "Missing answers:"]
+       [:p (s/join ", "
+                   (for [{:keys [number part]} missing]
+                     (str number part)))]]))))
 
 (defn render-scores [scores scores-el]
   (d/replace-contents! scores-el (scores-node scores))
